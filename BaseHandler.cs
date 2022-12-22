6250084a81afa17e0a0bbf3c68f2cba5b54c4ddc
NoClipMonster <iavedis9@gmail.com>
2022-12-22 21:46:41 +0300

    Release

---
+++ BaseHandler.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Windows;
+
+using Microsoft.Data.SqlClient;
+
+namespace DBHandler {
+    public class BaseHandler {
+        SqlConnection connection;
+        public bool isConnected { get { return connection.State == ConnectionState.Open; } }
+
+        public delegate void StateChangedHandler();
+        public event StateChangedHandler Connected;
+        public event StateChangedHandler Disconected;
+
+        public BaseHandler() {
+            connection = new SqlConnection();
+            connection.StateChange += Connection_StateChange;
+        }
+
+        private void Connection_StateChange(object sender, StateChangeEventArgs e) {
+            if ( e.CurrentState == ConnectionState.Open && Connected != null )
+                Connected();
+            else if ( e.CurrentState == ConnectionState.Closed && Disconected != null )
+                Disconected();
+        }
+
+
+        public async void Connect() {
+            string connectionString = "Server=(LocalDB)\\MSSQLLocalDB;DataBase=TestBase; Integrated Security=True;Connect Timeout=30";
+            connection.ConnectionString = connectionString;
+            try {
+                await connection.OpenAsync();
+            }
+            catch ( SqlException ex ) {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+        }
+        public async void Disconnect() {
+            try {
+                await connection.CloseAsync();
+            }
+            catch ( SqlException ex ) {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+        }
+        public List<string> GetFields() {
+            List<string> fields = new List<string>();
+            string sqlExpression = "SELECT DISTINCT (SELECT Name FROM Products WHERE Products.Id = Pairs.ProductId), (SELECT Name FROM Categories WHERE Categories.Id = Pairs.CategoryId) FROM Pairs";
+            //"(SELECT Name FROM Categories WHERE Categories.Id = Pairs.CategoryId) AS C" +
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            if ( reader.HasRows ) // если есть данные
+            {
+                while ( reader.Read() ) // построчно считываем данные
+                {
+                    fields.Add(reader.GetValue(0).ToString() + reader.GetValue(1).ToString());
+                }
+            }
+            return fields;
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+using Microsoft.Win32;
+
+namespace DBHandler {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        BaseHandler myBase = new BaseHandler();
+
+        public MainWindow() {
+            InitializeComponent();
+            myBase.Connected += MyBase_Connected;
+            myBase.Disconected += MyBase_Disconected;
+        }
+
+        private void MyBase_Disconected() {
+            Dispatcher.Invoke(() => {
+                ConnectBT.IsEnabled = true;
+                ConnectBT.Content = "Подключить";
+                StateIndcator.Fill = new SolidColorBrush(new Color() { R = 200, G = 70, B = 70, A = 255 });
+            });
+        }
+
+        private void MyBase_Connected() {
+            Dispatcher.Invoke(() => {
+                ConnectBT.IsEnabled = true;
+                ConnectBT.Content = "Отключить";
+                StateIndcator.Fill = new SolidColorBrush(new Color() { R = 70, G = 200, B = 70, A = 255 });
+            });
+        }
+
+        private void ConnectBT_Click(object sender, RoutedEventArgs e) {
+            ConnectBT.IsEnabled = false;
+            if ( myBase.isConnected )
+                myBase.Disconnect();
+            else
+                myBase.Connect();
+        }
+
+        private void UpdateBT_Click(object sender, RoutedEventArgs e) {
+
+            dataCB.Items.Clear();
+            foreach ( var item in myBase.GetFields() ) {
+                dataCB.Items.Add(item);
+            }
+           
+        }
+    }
+}
